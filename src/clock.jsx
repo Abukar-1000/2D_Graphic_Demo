314bc92af11bb464553941bfb465ed0c4145cebe
Abukar-1000 <abukardjama1@gmail.com>
2023-05-19 12:26:14 -0400

    text and image added

--- src/clock.jsx
+++ src/clock.jsx
@@ -1,23 +1,49 @@
 /*
     This is a canvas that will dynamically draw a new clock onto the screen
 */
-import { useEffect } from "react"
+
+import { React, useEffect } from "react"
+
+function testDraw(con){
+    con.fillStyle = "red";
+    con.font = "20pt sans-serif";
+    con.fillText("Canvas Rocks!", 200, 300);
+}
+
 
 function Clock(props){
 
-    // insert the image
+    const drawClockImg = () =>{
+        let clock = document.getElementById("clockCanvas");
+        let clockImage = document.querySelector(".clockImage");
+        let con = clock.getContext("2d");
+        con.drawImage(clockImage, 0, 0, 1200, 500);
+        // hide the image after we grab the contents
+        clockImage.classList.add("hidden");
+    }
+    // draw the clock image on the canvas
     useEffect(() => {
         let clock = document.getElementById("clockCanvas");
-        let image = new Image();
-        image.src = "clockBaseImg.jpg";
-
-        console.log(clock)
-        let con = clock.getContext("2D");
-        con.drawImage(image);
+        let con = clock.getContext("2d");
+        testDraw(con);
+        drawClockImg();
+        // if the page gets reloaded, then reload the image as {disply: none} removes it from the dom, which wont let me query it after
+        // going to remove the .hidden so I can query for the image
+        window.onunload = (e) => {
+            let clockBody = document.querySelector(".clockDiv");
+            clockBody.innerHTML = `
+            <img src="clockBaseImg.png" className="clockImage" alt="clock image"/>
+            <canvas id="clockCanvas" width = "1200" height = "500" ></canvas>
+            `;
+        }
+        window.onload = e => {
+            drawClockImg();
+        }
     })
-    return <>
-        <canvas id={"clockCanvas"} width = {"1200"} height = {"500"} ></canvas>
-    </>
+    return <div className="clockDiv">
+        <img src="clockBaseImg.png" className="clockImage" alt="clock image"/>
+        <canvas id="clockCanvas" width = "1200" height = "500" ></canvas>
+    </div>
 }
 
 
---
+++ src/numbers.js
@@ -0,0 +1,17 @@
+
+
+
+/*
+    Going to take clock image canvas element in the constructor.
+    With this it will draw the current time onto the clock image canvas.
+    Will also contain the date as text, and will add it to the clock canvas as well. 
+*/
+
+class ClockDate {
+
+    #canvasElement = null;
+    constructor(element){
+        this.#canvasElement = element;
+    }
+    
+}
